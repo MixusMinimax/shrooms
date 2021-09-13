10888b61c3bb1bb940cc385dfa41ec45783c211f
Maxi <maxi.barmetler@gmail.com>
2021-09-13 14:33:21 +0200

    vue create

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Hello World!</h1>
+  <Example />
+</template>
+
+<script>
+import Example from './components/Example.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Example,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
